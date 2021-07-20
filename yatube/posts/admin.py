912d8f41f471aecc07b1098c700a3ed9fb0d7146
Andronik Maria <andronikmar@yandex.com>
2021-07-20 20:39:25 +0300

    Выполнено задание проекта

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post, Group
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    """Класс для настройки отображения модели в интерфейсе админки."""
+
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
+    list_editable = ('group',)
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 # Create your views here.
-from django.http import HttpResponse
-
-
 # Главная страница
-def index(request):    
-    template = 'posts/index.html'
-    return render(request, template)
+def index(request):
+    """View - функция для главной страницы проекта."""
+
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title_index = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title_index': title_index,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 # Страница со списком постов
 def group_posts(request, slug):
-    return HttpResponse(f'Посты {slug}')
+    """View - функция для страницы с постами, отфильтрованными по группам."""
 
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title_group = 'Записи сообщества'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title_group': title_group,
 
+    }
+    return render(request, 'posts/group_list.html', context)
